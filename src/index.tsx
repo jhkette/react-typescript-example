e1e1fce40e767331c07d91a7d0499bf77e007d21
khenryjoe <joseph.ketterer@gmail.com>
2020-07-13 10:30:11 +0100

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,44 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+interface AppProps {
+    color: string
+}
+interface AppState {
+    counter: number
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// interfaces here are added as generics
+// We need to add a props interface as a generic
+// We need to add to add a state interface
+// We only need to add an interface as its extennding the
+// react component class and we are using a constructor 
+// to accesss the state property in the parent class - that expects
+// a class - it needs to be described by a generic object S
+class App extends React.Component<AppProps, AppState> {
+    constructor(props: AppProps) {
+        super(props);
+        this.state = { counter: 0 }
+    }
+
+    onIncrement = (): void => {
+        this.setState({ counter: this.state.counter + 1 })
+    }
+    onDecrement = (): void => {
+        this.setState({ counter: this.state.counter - 1 })
+    }
+    render() {
+        return <div>
+            <p>{this.state.counter}</p>
+            <button onClick={this.onIncrement}>Increment</button>
+            <button onClick={this.onDecrement}>Decrement</button>
+
+        </div>
+    }
+}
+
+
+ReactDom.render(
+    <App color="red" />,
+    document.querySelector("#root")
+)
